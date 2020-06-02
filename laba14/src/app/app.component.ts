22fca23a35e93d0404d2ee7793bfc1847fcd9b1a
Tatana <tatananedosekina@gmail.com>
2020-06-02 17:19:55 +0300

    first commit

---
+++ laba14/src/app/app.component.ts
@@ -0,0 +1,71 @@
+import { Component, OnInit} from '@angular/core';
+import {
+  MyWorker,
+  MyWorkersDatabase,
+  MyWorkerType,
+} from './shared/worker.model';
+import { isNullOrUndefined } from 'util';
+import { WorkersService } from './shared/workers.service';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+
+
+
+
+export class AppComponent implements OnInit{
+  title = 'Список сотрудников';
+  workers: any;
+  myWorkerType = MyWorkerType;
+
+  constructor(private workerService: WorkersService) {
+
+  }
+
+  async ngOnInit() {
+    
+    this.getData();
+  }
+
+  getByType(type: number) {
+    return this.workers.filter((worker) => worker.type === type);
+  }
+
+  async onDeleteById(event:number) {
+    try {
+      
+      await this.workerService.deleteWorker(event);   
+      } catch (err) {
+        console.error(err);
+      } finally {
+        this.getData(); 
+      }
+  }
+
+  async onAddWorker(event: MyWorker) {
+    try {
+      console.log(event);
+      await this.workerService.postWorker(event);
+    } catch (err) {
+      console.error(err);
+    } finally {
+      this.getData();
+    }
+  }
+  
+  
+
+  async getData() {
+
+    try {
+
+      this.workers = await this.workerService.getWorker();
+
+    } catch (err) {
+      console.error(err);
+    }
+  }
+}
---
+++ laba14/src/app/ui/table-workers/table-workers.component.ts
@@ -0,0 +1,61 @@
+import { Component, OnInit, Input, Output, EventEmitter } from '@angular/core';
+import { MyWorker, MyWorkerType } from 'src/app/shared/worker.model';
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
+
+@Component({
+  selector: 'app-table-workers',
+  templateUrl: './table-workers.component.html',
+  styleUrls: ['./table-workers.component.css'],
+})
+export class TableWorkersComponent implements OnInit {
+  @Input() title: string;
+  @Input() workers: MyWorker[] = [];
+  myWorkerType = MyWorkerType;
+  editWorker = -1;
+  name;
+  surname;
+  phone;
+  type = 0;
+  
+
+  @Output() deleteWorker = new EventEmitter<number>();
+
+  constructor(
+    
+    ) {}
+
+  ngOnInit(): void {
+
+  }
+
+  onDeleteWorker(id: number) {
+    this.deleteWorker.emit(id);
+  }
+
+  onEdit(id){
+    let w = this.workers.find(worker => worker.id === id);
+    console.log(w);
+    this.editWorker = w.id;
+    this.name = w.name;
+    this.surname = w.surname;
+    this.phone = w.phone;
+    //console.log(this.name, this.surname, this.workers[this.editWorker-1]);
+  }
+  
+  onEditWorker(id){
+    if(this.name==null||this.name==""||this.surname==null||this.surname==""){
+      return;
+      }
+      else{
+        let w = this.workers.find(worker => worker.id === id);
+        let worker = w;
+        worker.name = this.name;
+        worker.surname = this.surname;
+        worker.phone = this.phone;
+        worker.type = this.type;
+        this.editWorker = -1;
+        
+      }
+  }
+ 
+}
